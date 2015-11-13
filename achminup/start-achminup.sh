2faa8186abbfca330c75cbf9ab3d77c05b468009
bqqbarbhg <samuli.1995@hotmail.com>
2015-11-13 11:43:46 +0200

    Typo fix

--- achminup/start-achminup.sh
+++ achminup/start-achminup.sh
@@ -1,4 +1,4 @@
-echo "<%php \$base_uri = '$LAYERS_API_URI/achminup/';" > /srv/www/achmiup/secrets.php
+echo "<%php \$base_uri = '$LAYERS_API_URI/achminup/';" > /srv/www/achminup/secrets.php
 
 service php5-fpm start
 

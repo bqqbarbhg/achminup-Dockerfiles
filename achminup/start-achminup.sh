c551d461d2fcb6a293532c9872798ef46cff6877
bqqbarbhg <samuli.1995@hotmail.com>
2015-11-13 14:29:59 +0200

    Fix startup script

--- achminup/start-achminup.sh
+++ achminup/start-achminup.sh
@@ -1,5 +1,6 @@
-echo "<%php \$base_uri = '$LAYERS_API_URI/achminup/';" > /srv/www/achminup/secrets.php
+echo "<?php \$base_uri = '$LAYERS_API_URI/achminup/';" > /srv/www/achminup/secrets.php
 
 service php5-fpm start
 
-nginx
+exec nginx
+

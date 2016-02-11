7ff34ed445e26879d781f733949b39feaf7e771f
bqqbarbhg <samuli.1995@hotmail.com>
2016-02-11 15:45:38 +0200

    Add transcoding

--- achminup/start-achminup.sh
+++ achminup/start-achminup.sh
@@ -2,5 +2,9 @@ echo "<?php \$base_url = '$LAYERS_API_URI' . 'achminup/';" > /srv/www/achminup/s
 
 service php5-fpm start
 
+cd /srv/www/achminup
+mkdir -p temp/transcode_work
+./transcode_watcher.py temp/videos_to_transcode temp/transcode_work videos &
+
 exec nginx
 
